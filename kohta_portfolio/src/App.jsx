b7e497ed4c5347ad705b90837964c9252d877437
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-11 16:50:15 +0900

    add header

--- kohta_portfolio/src/App.jsx
+++ kohta_portfolio/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <header className='text-grey-700 border-b border-grey-200'>
+      <div className='flex container  mx-auto p-5 flex-col md:flex-row items-center'>
+        <a href="#" className='font-medium test-grey-900 mb-4 md:mb-0'>
+          <span className='text-xl ml-3'>Kohta Portdolio</span>
+        </a>
+        <nav className='md:ml-auto test-base'>
+          <a href="#home" className='mr-5 hover:text-blue-400 duration-300'>HOME</a>
+          <a href="#about" className='mr-5 hover:text-blue-400 duration-300'>ABOUT</a>
+          <a href="#skills" className='mr-5 hover:text-blue-400 duration-300'>SKILLS</a>
+          <a href="#blog" className='mr-5 hover:text-blue-400 duration-300'>BLOG</a>
+          <a href="#blog" >CONTACT</a>
+        </nav>
+      </div>
+    </header>
   )
 }
 
